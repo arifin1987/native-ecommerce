76072866792d4cade33f04f2f0c793711e2acc78
Shohidul Arifin <arifinshohidul@gmail.com>
2026-02-26 21:43:54 -0800

    initial commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,22 @@
 import express from "express";
+import path from "path" ;
+import { ENV } from "./config/env.js";
 
 const app = express();
+const __dirname = path.resolve();
 
 app.get("/api/health", (req,res)=>{
     res.status(200).json({message:"Success"})
 })
+// Make our app ready for deployment
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../admin/dist")))
+    app.get("/{*any}", (req,res)=>{
+        res.sendFile(path.join(__dirname,"../admin","dist","index.html"));
 
-app.listen(5000, ()=>{
-    console.log("app is up and running on port 5000")
+    })
+}
+
+app.listen(ENV.PORT, ()=>{
+    console.log("server is up and running ")
 })
